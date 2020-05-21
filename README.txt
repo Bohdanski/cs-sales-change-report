79168a6bbab91242b6aa07865c23703294074583
Bohdan Tkachenko <bohdantk@buffalo.edu>
2020-05-21 11:29:19 -0400

    - tests.py: Will now bring the export file from SQL Server containing Category and Brand.

--- README.txt
+++ README.txt
@@ -22,6 +22,8 @@ step 3 - Double click the "launcher.bat" file
 
 v0.1 - 04/21/2020 - Beta release that currently complies half of the report in order to speed up manual work
 v0.2 - 05/13/2020 - Updated beta version with additional fields. Program output is ready to be pasted into the template without additional manipulation required
+v0.3 - 05/13/2020 - Updated beta version to now take in a mic item dump flat file, to poplulate the category field
+
 
 
 # Author
--- tests.py
+++ tests.py
@@ -56,11 +56,23 @@ def main():
             for (index, sheet) in enumerate(sheet_list):
                 cs_list.append(workbook.parse(index, skiprows=1, header=None))
 
+        txt_list = glob.glob(data_dir + "*.txt")
+
+        for txt_file in txt_list:
+            if fnmatch.fnmatch(txt_file.lower(), "*mic*.txt") == True:
+                df_mic = pd.read_csv(txt_file, sep="|", skiprows=1, header=None)
+                df_mic.columns = [3, 1, 24]
+                df_mic.drop(columns=[1], inplace=True)
+
         df_cs_data = pd.concat(cs_list)
+        df_cs_data = df_cs_data.merge(df_mic, how="left", on=3)
+        df_cs_data.drop_duplicates(inplace=True)
+
         df_cs_data[5] = ["Rusty Ames" if x == "Rusty Amees" else x for x in df_cs_data[5]]
         df_cs_data[3] = df_cs_data[3].map('{:0>6}'.format)
         df_cs_data[6] = df_cs_data[6].map('{:0>6}'.format)
-        df_cs_data = df_cs_data.reindex(df_cs_data.columns.tolist() + [24, 25, 26, 27, 28, 29, 30], axis=1)
+
+        df_cs_data = df_cs_data.reindex(df_cs_data.columns.tolist() + [25, 26, 27, 28, 29, 30], axis=1)
         df_cs_data = df_cs_data[[0, 1, 2, 3, 4, 5, 24, 22, 23, 6, 7, 8, 25, 9, 10, 11, 26, 27, 12, 13, 14, 28, 15, 16, 17, 18, 19, 20, 21, 29, 30]]
         df_cs_data.columns = ['GL',
                               'Location Name',
@@ -92,6 +104,7 @@ def main():
                               'Future Bookings',
                               'Item Key',
                               'Manufacturer Status']
+
         df_cs_data.to_excel(f"{archive_dir}CS-Sales-Change-{create_timestamp()}.xlsx", index=False)
 
         exit()
